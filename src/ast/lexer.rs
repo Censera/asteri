a9628d03f0e07e3db1814af12f4393428e837a87
Censera <Censera@proton.me>
2026-03-30 22:41:20 +0100

    A lexer. The program panics becuase of an overflow,

---
+++ src/ast/lexer.rs
@@ -0,0 +1,116 @@
+#[derive(Debug)]
+pub enum TokenKind {
+  Astri,
+  Slash,
+  Plus,
+  Minus,
+  RightParen,
+  LeftParen,
+  Number(i64),  
+  Unknown,
+  EOF,
+}
+
+#[derive(Debug)]
+pub struct TextSpan {
+  start:  usize,
+  end:    usize,
+  literal:  String,
+}
+
+impl TextSpan {
+  pub fn new(start: usize, end: usize, literal: String) -> Self {
+    Self {
+      start,
+      end,
+      literal
+    }
+  }
+
+  pub fn len(&self) -> usize {
+    self.start - self.end
+  }
+}
+
+#[derive(Debug)]
+pub struct Token {
+  kind: TokenKind,
+  span: TextSpan,
+}
+
+impl Token {
+  pub fn new(kind: TokenKind, span: TextSpan) -> Self {
+    Self {
+      kind,
+      span
+    }
+  }
+}
+
+pub struct Lexer<'a> {
+  input: &'a str,
+  current_pos: usize,
+}
+
+impl <'a> Lexer<'a> {
+  pub fn new(input: &'a str) -> Self {
+    Self { input, current_pos: 0 }
+  }
+
+  pub fn next_token(&mut self) -> Option<Token> {
+    
+    if self.current_pos > self.input.len() {
+      return None;
+    }
+
+    if self.current_pos == self.input.len() {
+      self.current_pos += 1;
+      return Some(Token::new(
+          TokenKind::EOF,
+          TextSpan::new(0, 0, '\0'.to_string())
+      ));
+    }
+
+    let start: usize = self.current_pos;
+    let c: char = self.current_char();
+    let mut kind = TokenKind::Unknown;
+    if Self::is_number_start(&c) {
+      let number: i64 = self.consume_number();
+      kind = TokenKind::Number(number);
+    }
+    
+    let end: usize = self.current_pos;
+    let literal: String = self.input[start..end].to_string();
+    let span  =  TextSpan::new(start, end, literal);
+    Some(Token::new(kind, span))
+  }
+  
+  fn is_number_start(c: &char) -> bool {
+    c.is_digit(10)
+  }
+  fn current_char(&self) -> char {
+    self.input.chars().nth(self.current_pos).unwrap()
+  }
+  fn consume(&mut self) -> Option<char> {
+    let c: char = self.current_char();
+    if self.current_pos >= self.input.len() {
+      return None;
+    }
+    Some(c)
+  }
+
+  fn consume_number(&mut self) -> i64 {
+    let mut number: i64 = 0;
+    while let Some(c) = self.consume() {
+      if c.is_digit(10) {
+        number = number * 10 + c.to_digit(10).unwrap() as i64;
+      } else { 
+          break;
+        }
+      }
+    number
+  }
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod ast;
+
 fn main() {
-    println!("Hello, world!");
+    let input = "4";
+
+    let mut lexer = ast::lexer::Lexer::new(input);
+    let mut tokens = Vec::new();
+    while let Some(token) = lexer.next_token() {
+      tokens.push(token);
+    }
+   println!("{:?}", tokens);
 }
