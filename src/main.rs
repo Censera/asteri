d46cf079c6fa2e632a8c752ca4f8a6b9588e0932
Censera <Censera@proton.me>
2026-06-07 18:40:21 +0100

    Commit #35

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::time::{Duration, Instant};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start_time = Instant::now();
-    let is_debug = false;
+    let is_debug = true;
     let args: Vec<String> = args().collect();
 
     if args.len() < 2 {
