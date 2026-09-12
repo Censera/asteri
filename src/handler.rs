9f3b99a93fc725ee1d337efc8660feef66ceee92
Censera <Censera@proton.me>
2026-09-12 07:49:38 +0100

    Align Astery handler with return API

--- src/handler.rs
+++ src/handler.rs
@@ -12,25 +12,30 @@ impl<'ctx> Handler<'ctx> {
         context: &'ctx Context,
         function: &an_inkwell::Function<'ctx>,
     ) -> Result<Self, an_inkwell::Error> {
+        if !std::ptr::eq(context.as_raw(), function.context()) {
+            return Err(an_inkwell::Error::DifferentContext);
+        }
+
         let builder = context.as_raw().builder()?;
         let block = function.block("entry")?;
         builder.position(&block)?;
         Ok(Self { context, builder })
     }
 
-    pub fn return_void(&self) -> Result<Value<'ctx>, an_inkwell::Error> {
-        self.builder.ret_void()
+    pub fn return_void(&self) -> Result<(), an_inkwell::Error> {
+        self.builder.ret_void();
+        Ok(())
     }
 
-    pub fn return_i32(&self, value: i32) -> Result<Value<'ctx>, an_inkwell::Error> {
+    pub fn return_i32(&self, value: i32) -> Result<(), an_inkwell::Error> {
         let ty = Type::i32(self.context.as_raw());
         let value = Value::integer(&ty, value as u64, true);
-        self.builder.ret(&value)
+        self.builder.ret(&value).map(|_| ())
     }
 
-    pub fn return_i64(&self, value: i64) -> Result<Value<'ctx>, an_inkwell::Error> {
+    pub fn return_i64(&self, value: i64) -> Result<(), an_inkwell::Error> {
         let ty = Type::i64(self.context.as_raw());
         let value = Value::integer(&ty, value as u64, true);
-        self.builder.ret(&value)
+        self.builder.ret(&value).map(|_| ())
     }
 }
