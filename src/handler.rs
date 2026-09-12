f568068703c20da2d743079b30f86ea3500d61fa
Censera <Censera@proton.me>
2026-09-12 05:55:21 +0100

    Keep handler context explicit

--- src/handler.rs
+++ src/handler.rs
@@ -4,6 +4,7 @@ use inkwell::values::FunctionValue;
 use crate::Context;
 
 pub struct Handler<'ctx> {
+    context: &'ctx Context,
     builder: inkwell::builder::Builder<'ctx>,
 }
 
@@ -12,7 +13,7 @@ impl<'ctx> Handler<'ctx> {
         let builder = context.as_raw().create_builder();
         let block = context.as_raw().append_basic_block(function, "entry");
         builder.position_at_end(block);
-        Self { builder }
+        Self { context, builder }
     }
 
     pub fn return_void(&self) -> Result<(), BuilderError> {
@@ -20,12 +21,20 @@ impl<'ctx> Handler<'ctx> {
     }
 
     pub fn return_i32(&self, value: i32) -> Result<(), BuilderError> {
-        let value = self.builder.get_insert_block().unwrap().get_context().i32_type().const_int(value as u64, true);
+        let value = self
+            .context
+            .as_raw()
+            .i32_type()
+            .const_int(value as u64, true);
         self.builder.build_return(Some(&value)).map(|_| ())
     }
 
     pub fn return_i64(&self, value: i64) -> Result<(), BuilderError> {
-        let value = self.builder.get_insert_block().unwrap().get_context().i64_type().const_int(value as u64, true);
+        let value = self
+            .context
+            .as_raw()
+            .i64_type()
+            .const_int(value as u64, true);
         self.builder.build_return(Some(&value)).map(|_| ())
     }
 }
